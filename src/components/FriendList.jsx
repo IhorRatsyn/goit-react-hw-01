4dfcf2d819ae85bef43b7be0d914930a53515db3
IhorRatsyn <ratsinjob@gmail.com>
2024-02-10 23:37:24 +0200

    Profile and FriendList module css creation

--- src/components/FriendList.jsx
+++ src/components/FriendList.jsx
@@ -1,5 +1,5 @@
 import FriendListItem from './FriendListItem';
-import styles from '../components/FriendListItem.module.css';
+import styles from '../components/FriendList.module.css';
 
 const FriendList = ({ friends }) => {
   return (
